f28325e7f80a0c6b1bd309e6bb8d25a508d8ad92
HoLLy <dario.db@hotmail.com>
2018-09-20 20:47:06 +0200

    Add project files.

---
+++ PokeOneToolkit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokeOneToolkit
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
